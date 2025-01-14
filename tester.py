ea5a22000d2bd17c181a8d61a2983c5efb80d2c4
Rafayel Latif <rafayel.latif@gmail.com>
2025-01-14 16:03:38 +0000

    Testing

--- tester.py
+++ tester.py
@@ -1,24 +1,37 @@
 
-import json
 import ao_core as ao
 from arch__tester import arch
 import numpy as np
 
 agent = ao.Agent(arch)
+for i in range(4):
+    agent.reset_state()
+    agent.reset_state(training=True)
 
 num_binary_digits = 5
 
-with open('cache_genre.json') as file:
-    data = json.load(file)
-bucket_id = data["buckets"]["Comedy"]["id"]
+def get_binary(id):
+    bucket_binary = np.array(list(np.binary_repr(id, num_binary_digits)), dtype=int)
+    blist = bucket_binary.tolist()
 
-bucket_binary = np.array(list(np.binary_repr(bucket_id, num_binary_digits)), dtype=int)
-Input_to_agent = bucket_binary.tolist()
-print(len(data["buckets"]))
-bucket_names = list(data["buckets"].keys())
-print(bucket_names)
+    return blist 
 
 
 
-agent.next_state(Input_to_agent, print_result=True)
+
+while True:
+    genre_id = int(input("Genre id: "))
+    theme_id = int(input("theme id; "))
+    genre_binary = get_binary(genre_id)
+    theme_binary = get_binary(theme_id)
+
+    Input_to_agent = genre_binary + theme_binary
+    agent.next_state(Input_to_agent, print_result=True)
+
+    recommendation_result = input("Did you like the recommendation (y/n): ")
+
+    if recommendation_result == "y":
+        agent.next_state(Input_to_agent, LABEL=[1], print_result=True)
+    else:
+        agent.next_state(Input_to_agent, LABEL=[0], print_result=True)
 
